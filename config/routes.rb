c672c54afd5fbe7e4f656964d44f9a0b9c5e88f0
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-10-12 23:12:28 -0500

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     get '/hello', to: 'application#hello_world'
   end
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
